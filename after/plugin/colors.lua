21731bf134022d27efda58633fd42ae44a591df8
Nathan Stilwell <nathan.stilwell@biltrewards.com>
2024-08-07 16:50:58 -0400

    Swapping Packer out for LazyVim
    Removed packer and associated files and replaced with `lazy.lua`
    (bootstrap and init) along with _lazy plugin spec_ files in `plugins`.
    Also dropping files in `after/` directory and putting setup in the
    individula lazy spec files (see treesitter "opts" and telescope "keys")

--- after/plugin/colors.lua
+++
@@ -1,8 +0,0 @@
--- Mimicking the Primeagen for starters
--- I don't know why this is necessary or really what 'after' does at all
-function ApplyColor(color)
-  color = color or "dracula"
-  vim.cmd.colorscheme(color)
-end
-
-ApplyColor()
---
+++ lua/nathanstilwell/plugins/dracula.spec.lua
@@ -0,0 +1,9 @@
+return {
+  {
+    "Mofiqul/dracula.nvim",
+    name = "dracula",
+    init = function()
+      vim.cmd("colorscheme dracula")
+    end,
+ },
+}
