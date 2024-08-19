dba48c29bcf801a627bea59ac6f46a0caaf10103
Nathan Stilwell <nathanstilwell@gmail.com>
2024-08-19 16:04:18 -0400

    *.spec files don't seem to work on OSX 12

--- lua/nathanstilwell/plugins/dracula.lua
+++ lua/nathanstilwell/plugins/dracula.lua
@@ -10,11 +10,11 @@
 --  ░                         ░
 
 return {
-  {
-    "Mofiqul/dracula.nvim",
-    name = "dracula",
-    init = function()
-      vim.cmd("colorscheme dracula")
-    end,
- },
+    {
+        "Mofiqul/dracula.nvim",
+        name = "dracula",
+        init = function()
+            vim.cmd("colorscheme dracula")
+        end,
+    },
 }
