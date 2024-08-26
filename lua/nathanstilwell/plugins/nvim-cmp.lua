a90fa1f04b54073fb5e49dc63320e28f011eca05
Nathan Stilwell <nathan.stilwell@biltrewards.com>
2024-08-26 17:53:53 -0400

    breaking up lsp-zero file

---
+++ lua/nathanstilwell/plugins/nvim-cmp.lua
@@ -0,0 +1,36 @@
+local cmp_opts = function(_, opts)
+  local cmp = require('cmp')
+
+  opts.sources = cmp.config.sources({
+    { name = 'nvim_lsp' },
+    { name = 'luasnip' },
+  }, {
+    { name = 'buffer' },
+  })
+
+  opts.mapping = cmp.mapping.preset.insert({
+    ['<C-Space>'] = cmp.mapping.complete(),
+    ['<C-u>'] = cmp.mapping.scroll_docs(-4),
+    ['<C-d>'] = cmp.mapping.scroll_docs(4),
+    ['<CR>'] = cmp.mapping.confirm({select = false}),
+  })
+
+  opts.snippet = {
+    expand = function(args)
+      vim.snippet.expand(args.body)
+    end
+  }
+
+  return opts
+end
+
+return {
+  {
+    'hrsh7th/nvim-cmp',
+    event = 'InsertEnter',
+    dependencies = {
+      {'L3MON4D3/LuaSnip'},
+    },
+    opts = cmp_opts
+  },
+}
