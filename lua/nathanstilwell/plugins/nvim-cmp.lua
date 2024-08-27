ca2c7ee0e50a6f816505c99c7563fd7f9cf68b5c
Nathan Stilwell <nathan.stilwell@biltrewards.com>
2024-08-27 13:27:34 -0400

    adding lspkind completion (and tweaks)

--- lua/nathanstilwell/plugins/nvim-cmp.lua
+++ lua/nathanstilwell/plugins/nvim-cmp.lua
@@ -1,21 +1,43 @@
 local cmp_opts = function(_, opts)
   local cmp = require('cmp')
+  -- local cmp_action = require('lsp-zero').cmp_action()
+  local lspkind = require('lspkind')
 
-  opts.sources = cmp.config.sources({
-    { name = 'nvim_lsp' },
-  })
+  opts.completion = {
+    completeopt = 'menu,menuone,noinsert'
+  }
+
+  opts.formatting = {
+    fields = { 'abbr', 'kind', 'menu' },
+    format = lspkind.cmp_format({
+      mode = 'symbol', -- show only symbol annotations
+      maxwidth = 100,
+      ellipsis_char = '…'
+    })
+  }
+
+  opts.preselect = 'item'
 
   opts.mapping = cmp.mapping.preset.insert({
+    ['<Tab>'] = cmp.mapping.confirm({ select = false }),
     ['<C-Space>'] = cmp.mapping.complete(),
     ['<C-u>'] = cmp.mapping.scroll_docs(-4),
     ['<C-d>'] = cmp.mapping.scroll_docs(4),
-    ['<CR>'] = cmp.mapping.confirm({select = false}),
+  })
+
+  opts.sources = cmp.config.sources({
+    { name = 'nvim_lsp' },
   })
 
   opts.snippet = {
     expand = function(args)
       vim.snippet.expand(args.body)
     end
+  }
+
+  opts.window = {
+    completion = cmp.config.window.bordered(),
+    documentation = cmp.config.window.bordered(),
   }
 
   return opts
@@ -26,7 +48,8 @@ return {
     'hrsh7th/nvim-cmp',
     event = 'InsertEnter',
     dependencies = {
-      {'L3MON4D3/LuaSnip'},
+      { 'L3MON4D3/LuaSnip' },
+      { 'onsails/lspkind.nvim' },
     },
     opts = cmp_opts
   },
