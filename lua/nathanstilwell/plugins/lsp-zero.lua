c83254d76cffbb08810a2c6ca8700813582e54db
Nathan Stilwell <nathan.stilwell@biltrewards.com>
2024-08-27 11:15:35 -0400

    lsp_zero: format on save

--- lua/nathanstilwell/plugins/lsp-zero.lua
+++ lua/nathanstilwell/plugins/lsp-zero.lua
@@ -1,10 +1,23 @@
-
 return {
   {
     'VonHeikemen/lsp-zero.nvim',
     branch = 'v4.x',
     lazy = true,
     config = false,
+    init = function()
+      local lsp_zero = require('lsp-zero')
+
+      lsp_zero.format_on_save({
+        format_opt = {
+          async = false,
+          timeout_ms = 10000,
+        },
+        servers = {
+          ['lua_ls'] = { 'lua' },
+          ['tsserver'] = { 'javascript', 'typescript' },
+        }
+      })
+    end,
   },
 
 }
