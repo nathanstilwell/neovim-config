7313119a133f9c8fea21e1b57c37b1c6a4904bc5
Nathan Stilwell <nathan.stilwell@biltrewards.com>
2024-08-27 11:15:19 -0400

    mason: ensure_installed

--- lua/nathanstilwell/plugins/mason.lua
+++ lua/nathanstilwell/plugins/mason.lua
@@ -3,10 +3,17 @@ return {
   {
     'williamboman/mason-lspconfig.nvim',
     opts = {
+      ensure_installed = {'tsserver', 'lua_ls', 'yamlls'},
       handlers = {
         function(server_name)
           require('lspconfig')[server_name].setup({})
         end,
+        -- Custom handlers can go here, example
+        -- tsserver = function()
+        --  require('lspconfig').tsserver.setup({
+        --      yadda yadda yadda
+        --  })
+        -- end
       },
     },
   },
