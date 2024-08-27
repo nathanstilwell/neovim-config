31cf363ae524e5c781188bb303093122749de6bd
Nathan Stilwell <nathan.stilwell@biltrewards.com>
2024-08-27 11:11:43 -0400

    SideQuest: Correcting syntax highlighting
    It seems I didn't have Treesitter set up correctly (the config options
    weren't being applied) and I had to make some accomodations for my weird
    file extensions in my dotfiles

--- lua/nathanstilwell/plugins/treesitter.lua
+++ lua/nathanstilwell/plugins/treesitter.lua
@@ -12,7 +12,6 @@ return {
     "nvim-treesitter/nvim-treesitter",
     build = ":TSUpdate",
     opts = {
-      -- A list of parser names, or "all"
       ensure_installed = {
         "c",
         "fish",
@@ -26,24 +25,10 @@ return {
         "vimdoc",
         "yaml",
       },
-
-      -- Install parsers synchronously (only applied to `ensure_installed`)
       sync_install = false,
-
-      -- Automatically install missing parsers when entering buffer
-      -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
       auto_install = true,
-
-      highlight = {
-        enable = true,
-
-       -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
-        -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
-        -- Using this option may slow down your editor, and you may see some duplicate highlights.
-        -- Instead of true it can also be a list of languages
-        additional_vim_regex_highlighting = false,
-      },
-    }
+      highlight = { enable = true, }
+    },
   },
   "nvim-treesitter/playground",
 }
