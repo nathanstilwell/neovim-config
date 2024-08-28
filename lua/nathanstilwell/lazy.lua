2d2d63d80978979537c8b17da5452b94b4aec549
Nathan Stilwell <nathan.stilwell@biltrewards.com>
2024-08-28 11:29:20 -0400

    Disabling the lazy change detection notification
    There's also some formatting that snuck in here.

--- lua/nathanstilwell/lazy.lua
+++ lua/nathanstilwell/lazy.lua
@@ -1,13 +1,13 @@
- -- ██▓    ▄▄▄      ▒███████▒▓██   ██▓      ██▒   █▓ ██▓ ███▄ ▄███▓
+-- ██▓    ▄▄▄      ▒███████▒▓██   ██▓      ██▒   █▓ ██▓ ███▄ ▄███▓
 -- ▓██▒   ▒████▄    ▒ ▒ ▒ ▄▀░ ▒██  ██▒     ▓██░   █▒▓██▒▓██▒▀█▀ ██▒
 -- ▒██░   ▒██  ▀█▄  ░ ▒ ▄▀▒░   ▒██ ██░      ▓██  █▒░▒██▒▓██    ▓██░
 -- ▒██░   ░██▄▄▄▄██   ▄▀▒   ░  ░ ▐██▓░       ▒██ █░░░██░▒██    ▒██
 -- ░██████▒▓█   ▓██▒▒███████▒  ░ ██▒▓░ ██▓    ▒▀█░  ░██░▒██▒   ░██▒
 -- ░ ▒░▓  ░▒▒   ▓▒█░░▒▒ ▓░▒░▒   ██▒▒▒  ▒▓▒    ░ ▐░  ░▓  ░ ▒░   ░  ░
 -- ░ ░ ▒  ░ ▒   ▒▒ ░░░▒ ▒ ░ ▒ ▓██ ░▒░  ░▒     ░ ░░   ▒ ░░  ░      ░
- --  ░ ░    ░   ▒   ░ ░ ░ ░ ░ ▒ ▒ ░░   ░        ░░   ▒ ░░      ░
- --    ░  ░     ░  ░  ░ ░     ░ ░       ░        ░   ░         ░
- --                 ░         ░ ░       ░       ░
+--  ░ ░    ░   ▒   ░ ░ ░ ░ ░ ▒ ▒ ░░   ░        ░░   ▒ ░░      ░
+--    ░  ░     ░  ░  ░ ░     ░ ░       ░        ░   ░         ░
+--                 ░         ░ ░       ░       ░
 
 -- Bootstrap lazy.nvim
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
@@ -17,7 +17,7 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
   if vim.v.shell_error ~= 0 then
     vim.api.nvim_echo({
       { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
-      { out, "WarningMsg" },
+      { out,                            "WarningMsg" },
       { "\nPress any key to exit..." },
     }, true, {})
     vim.fn.getchar()
@@ -27,4 +27,8 @@ end
 vim.opt.rtp:prepend(lazypath)
 
 -- Setup Plugins
-require("lazy").setup("nathanstilwell.plugins")
+require("lazy").setup("nathanstilwell.plugins", {
+  change_detection = {
+    notify = false,
+  },
+})
