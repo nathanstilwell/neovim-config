84c27d9462c4a3bb74755c82b465a4935f9808b3
Nathan Stilwell <nathan.stilwell@biltrewards.com>
2024-11-01 16:26:05 -0400

    Gitsigns options better defaults

--- lua/nathanstilwell/plugins/gitsigns.lua
+++ lua/nathanstilwell/plugins/gitsigns.lua
@@ -1,3 +1,30 @@
+--  ▄████  ██▓▄▄▄█████▓  ██████  ██▓  ▄████  ███▄    █   ██████
+-- ██▒ ▀█▒▓██▒▓  ██▒ ▓▒▒██    ▒ ▓██▒ ██▒ ▀█▒ ██ ▀█   █ ▒██    ▒
+--▒██░▄▄▄░▒██▒▒ ▓██░ ▒░░ ▓██▄   ▒██▒▒██░▄▄▄░▓██  ▀█ ██▒░ ▓██▄
+--░▓█  ██▓░██░░ ▓██▓ ░   ▒   ██▒░██░░▓█  ██▓▓██▒  ▐▌██▒  ▒   ██▒
+--░▒▓███▀▒░██░  ▒██▒ ░ ▒██████▒▒░██░░▒▓███▀▒▒██░   ▓██░▒██████▒▒
+-- ░▒   ▒ ░▓    ▒ ░░   ▒ ▒▓▒ ▒ ░░▓   ░▒   ▒ ░ ▒░   ▒ ▒ ▒ ▒▓▒ ▒ ░
+--  ░   ░  ▒ ░    ░    ░ ░▒  ░ ░ ▒ ░  ░   ░ ░ ░░   ░ ▒░░ ░▒  ░ ░
+--░ ░   ░  ▒ ░  ░      ░  ░  ░   ▒ ░░ ░   ░    ░   ░ ░ ░  ░  ░
+--      ░  ░                 ░   ░        ░          ░       ░
+
+local gitsigns_init = function()
+  local gitsigns = require("gitsigns")
+
+  gitsigns.setup({
+    signcolumn = true,
+    numhl = true,
+    current_line_blame = true,
+    linehl = false,
+    word_diff = false,
+    current_line_blame_opts = {
+      virt_text = true,
+      virt_text_pos = "right_align",
+      delay = 500,
+    },
+  })
+end
 return {
-  "lewis6991/gitsigns.nvim", opts = {},
+  "lewis6991/gitsigns.nvim",
+  init = gitsigns_init,
 }
