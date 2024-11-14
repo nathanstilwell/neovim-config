dfd8c68a90a980ce1254e45597e6a84ae10229d6
Nathan Stilwell <nathan.stilwell@biltrewards.com>
2024-11-14 17:01:48 -0500

    user command to insert figlet text

---
+++ lua/nathanstilwell/usercmds.lua
@@ -0,0 +1,27 @@
+-- ▄• ▄▌.▄▄ · ▄▄▄ .▄▄▄       ▄▄· • ▌ ▄ ·. ·▄▄▄▄  .▄▄ ·
+-- █▪██▌▐█ ▀. ▀▄.▀·▀▄ █·    ▐█ ▌▪·██ ▐███▪██▪ ██ ▐█ ▀.
+-- █▌▐█▌▄▀▀▀█▄▐▀▀▪▄▐▀▀▄     ██ ▄▄▐█ ▌▐▌▐█·▐█· ▐█▌▄▀▀▀█▄
+-- ▐█▄█▌▐█▄▪▐█▐█▄▄▌▐█•█▌    ▐███▌██ ██▌▐█▌██. ██ ▐█▄▪▐█
+--  ▀▀▀  ▀▀▀▀  ▀▀▀ .▀  ▀    ·▀▀▀ ▀▀  █▪▀▀▀▀▀▀▀▀•  ▀▀▀▀
+vim.api.nvim_create_user_command(
+  'Fig',
+  function(opts)
+    -- parse arguments
+    local fargs = opts.fargs
+    local font = fargs[1]
+    local text = table.concat(vim.list_slice(fargs, 2, #fargs), " ")
+
+    -- run figlet command, read output into lines
+    local handle = assert(io.popen("figlet -d ~/.local/share/figlet/fonts -w 120 -f " .. font .. " " .. text))
+    local result = handle:lines()
+    local lines = {}
+    for line in result do
+      table.insert(lines, line)
+    end
+    assert(handle:close())
+
+    --put lines into current buffer at cursor
+    vim.api.nvim_put(lines, "", false, true)
+  end,
+  { nargs = "*" }
+)
