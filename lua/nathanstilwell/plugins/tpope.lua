d47135376609d8c323c55add3cd20c28b386dbba
Nathan Stilwell <nathan.stilwell@biltrewards.com>
2025-01-15 21:52:15 -0500

    [POPE] The Pope has done it again by solving case conversion

--- lua/nathanstilwell/plugins/tpope.lua
+++ lua/nathanstilwell/plugins/tpope.lua
@@ -12,5 +12,6 @@ return {
   "tpope/vim-commentary",
   "tpope/vim-surround",
   "tpope/vim-fugitive",
+  "tpope/vim-abolish", -- All you need to know is that this converts any case to camelCase with `crc`!!
   "github/copilot.vim",
 }
