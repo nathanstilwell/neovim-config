33d275b3522826e48252ff97854e49c686c7f57e
Nathan Stilwell <nathan.stilwell@biltrewards.com>
2025-01-20 19:16:53 -0500

    Moar Pope

--- lua/nathanstilwell/plugins/tpope.lua
+++ lua/nathanstilwell/plugins/tpope.lua
@@ -13,5 +13,8 @@ return {
   "tpope/vim-surround",
   "tpope/vim-fugitive",
   "tpope/vim-abolish", -- All you need to know is that this converts any case to camelCase with `crc`!!
+  "tpope/vim-repeat",  -- Repeat tpope-isms
+  "tpope/vim-vinegar", -- Better netrw
+  "tpope/vim-jdaddy",  -- JSON manipulation
   "github/copilot.vim",
 }
