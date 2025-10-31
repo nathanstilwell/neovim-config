7ba4c45bf58295576c0039b4f56f37b0f6f4e16c
Nathan Stilwell <nathan.stilwell@bilt.com>
2025-10-31 13:59:04 -0400

    [tpope] giving myself a case coercion cheatsheet

--- lua/nathanstilwell/plugins/tpope.lua
+++ lua/nathanstilwell/plugins/tpope.lua
@@ -12,9 +12,16 @@ return {
   "tpope/vim-commentary",
   "tpope/vim-surround",
   "tpope/vim-fugitive",
-  "tpope/vim-abolish", -- All you need to know is that this converts any case to camelCase with `crc`!!
+  "tpope/vim-abolish", -- Case coercion and more
   "tpope/vim-repeat",  -- Repeat tpope-isms
   "tpope/vim-vinegar", -- Better netrw
   "tpope/vim-jdaddy",  -- JSON manipulation, `gqaj` to format JSON
   "github/copilot.vim",
 }
+
+
+-- Quick vim-abolish cheatsheet: (I'm only really here for the coercions)
+-- to camelCase -- crc
+-- to snake_case -- crs
+-- to UPPER_SNAKE_CASE -- cru
+-- to kebab-case -- cr-
