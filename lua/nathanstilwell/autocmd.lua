ae260b05ea2f70168d95a1676f6f9dd0ac0c1f7e
Nathan Stilwell <nathan.stilwell@bilt.com>
2026-07-23 12:52:18 -0400

    darken column marker

--- lua/nathanstilwell/autocmd.lua
+++ lua/nathanstilwell/autocmd.lua
@@ -22,7 +22,7 @@ vim.api.nvim_create_autocmd("TextYankPost", {
 vim.api.nvim_create_autocmd("ColorScheme", {
   pattern = "*",
   callback = function()
-    vim.api.nvim_set_hl(0, "ColorColumn", { bg = "#111111" })
+    vim.api.nvim_set_hl(0, "ColorColumn", { bg = "#010101" })
   end,
 })
 
